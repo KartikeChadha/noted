3a75017ed7ae03f39ee0b14b681838b8862d3be8
KartikeChadha <kartikech.z@gmail.com>
2022-06-10 13:18:01 +0530

    step-7

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:noted/constants/routes.dart';
+import 'package:noted/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -55,20 +54,41 @@ class _RegisterViewState extends State<RegisterView> {
                 final email = _email.text;
                 final password = _password.text;
                 try {
-                  final userCredential = await FirebaseAuth.instance
-                      .createUserWithEmailAndPassword(
+                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
                     email: email,
                     password: password,
                   );
-                  devtools.log(userCredential.toString());
+                  final user = FirebaseAuth.instance.currentUser;
+                  await user?.sendEmailVerification();
+                  // ignore: use_build_context_synchronously
+                  Navigator.of(context).pushNamed(verifyEmailRoute);
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'weak-password') {
-                    devtools.log('Weak password');
+                    await showErrorDialog(
+                      context,
+                      'Weak password',
+                    );
                   } else if (e.code == 'email-already-in-use') {
-                    devtools.log('Email is alreay in use');
+                    await showErrorDialog(
+                      context,
+                      'The email is already in use',
+                    );
                   } else if (e.code == 'invalid-email') {
-                    devtools.log('Email is invalid');
+                    await showErrorDialog(
+                      context,
+                      'Email entered is invalid',
+                    );
+                  } else {
+                    await showErrorDialog(
+                      context,
+                      'Error: ${e.code}',
+                    );
                   }
+                } catch (e) {
+                  await showErrorDialog(
+                    context,
+                    e.toString(),
+                  );
                 }
               },
               child: const Text('Register')),
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:noted/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -12,18 +13,34 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 207, 179, 255),
       appBar: AppBar(
         title: const Text('Verify Email'),
       ),
       body: Column(
         children: [
-          const Text('Please verify your email address'),
+          const Text(
+              "We've sent you a verification email. Please open it to verify your account"),
+          const Text(
+              "If you haven't recieved a verification email yet, please press the button below"),
           TextButton(
-              onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                await user?.sendEmailVerification();
-              },
-              child: const Text('Send Email Verification'))
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+            },
+            child: const Text('Send Email Verification'),
+          ),
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              // ignore: use_build_context_synchronously
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (_) => false,
+              );
+            },
+            child: const Text('Restart'),
+          )
         ],
       ),
     );
