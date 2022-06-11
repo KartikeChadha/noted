03a306a557bbd687bcb18cc08f948e2488b41a51
KartikeChadha <kartikech.z@gmail.com>
2022-06-11 18:22:04 +0530

    step-8

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -60,11 +60,22 @@ class _LoginViewState extends State<LoginView> {
                     email: email,
                     password: password,
                   );
-                  // ignore: use_build_context_synchronously
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (_) => false,
-                  );
+                  final user = FirebaseAuth.instance.currentUser;
+                  if (user?.emailVerified ?? false) {
+                    //email is verified
+                    // ignore: use_build_context_synchronously
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      notesRoute,
+                      (_) => false,
+                    );
+                  } else {
+                    //email is NOT verified
+                    // ignore: use_build_context_synchronously
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute,
+                      (_) => false,
+                    );
+                  }
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'user-not-found') {
                     await showErrorDialog(
